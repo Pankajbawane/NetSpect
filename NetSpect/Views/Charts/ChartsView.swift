3c4fff7f91cc3843021c02105aade2a8e5979d8b
Pankaj Bawane <->
2025-11-20 19:43:02 +0530

    arrange chart views

--- NetSpect/Views/Charts/ChartsView.swift
+++ NetSpect/Views/Charts/ChartsView.swift
@@ -5,44 +5,7 @@
 //  Created by Pankaj Bawane on 11/07/25.
 //
 
-import SwiftUI
-import Charts
-
-struct AnalyticsDashboardView: View {
-    
-    let data: [NWLogItem]
-    
-    var statusCode: [ChartParameter<Int>] {
-        NWChartItemFactory.get(items: data, key: \.statusCode)
-    }
-    
-    var httpMethod: [ChartParameter<String>] {
-        NWChartItemFactory.get(items: data, key: \.method)
-    }
-    
-    var hosts: [ChartParameter<String>] {
-        NWChartItemFactory.get(items: data, key: \.host)
-    }
-    
-    var body: some View {
-        ScrollView {
-            VStack {
-                Text("Status Code")
-                    .font(.caption)
-                    .fontWeight(.bold)
-                StatusCodeChartView(data: statusCode)
-                Text("HTTP Method")
-                    .font(.caption)
-                    .fontWeight(.bold)
-                HTTPMethodPieChartView(data: httpMethod)
-                Text("Host")
-                    .font(.caption)
-                    .fontWeight(.bold)
-                HostsChartView(data: hosts)
-            }
-        }
-    }
-}
+import Foundation
 
 struct NWChartItemFactory {
     
@@ -66,62 +29,4 @@ struct ChartParameter<T: Hashable>: Identifiable {
     let count: Int
     var id: T { value }
     var stringValue: String { "\(value)" }
-}
-
-struct StatusCodeChartView: View {
-    
-    let data: [ChartParameter<Int>]
-    
-    var body: some View {
-        Chart(data) {
-            BarMark(
-                x: .value("Status Code", $0.stringValue == "0" ? "Unknown" : $0.stringValue),
-                y: .value("Count", $0.count)
-            )
-            .foregroundStyle(by: .value("Status Code", $0.stringValue == "0" ? "Unknown" : $0.stringValue))
-        }
-        .chartYAxis {
-            AxisMarks(position: .leading)
-        }
-        .frame(height: 300)
-        .padding()
-    }
-}
-
-struct HTTPMethodPieChartView: View {
-    let data: [ChartParameter<String>]
-
-    var body: some View {
-        Chart(data) {
-            SectorMark(
-                angle: .value("Count", $0.count),
-                innerRadius: .ratio(0.4),
-                angularInset: 1
-            )
-            .foregroundStyle(by: .value("HTTP Method", $0.value))
-        }
-        .frame(height: 300)
-        .padding()
-    }
-}
-
-
-struct HostsChartView: View {
-    
-    let data: [ChartParameter<String>]
-    
-    var body: some View {
-        Chart(data) {
-            BarMark(
-                x: .value("Hosts", $0.value),
-                y: .value("Count", $0.count)
-            )
-            .foregroundStyle(by: .value("Hosts", $0.value))
-        }
-        .chartYAxis {
-            AxisMarks(position: .leading)
-        }
-        .frame(height: 300)
-        .padding()
-    }
 }
---
+++ NetSpect/Views/Charts/HTTPMethodPieChartView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import Charts
+
+struct HTTPMethodPieChartView: View {
+    let data: [ChartParameter<String>]
+
+    var body: some View {
+        Chart(data) {
+            SectorMark(
+                angle: .value("Count", $0.count),
+                innerRadius: .ratio(0.4),
+                angularInset: 1
+            )
+            .foregroundStyle(by: .value("HTTP Method", $0.value))
+        }
+        .frame(height: 300)
+        .padding()
+    }
+}
